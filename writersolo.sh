dba1e94dfaa80a249a868b8e88f36745196269e0
Matthew Polk <153921961+exjaynine@users.noreply.github.com>
2023-12-20 06:14:44 -0500

    Update writersolo.sh

--- writersolo.sh
+++ writersolo.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
-#Note to self: further additions to this shell script wrapper may be needed, but it's fine as is for right now.
-zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop'
+#If you are on wayland, lets allow this to use native wayland instead of the xwayland system
+#Else, just use the X11 backend.
+if [ -z "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
+  zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop/' --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
+else
+  zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop' "$@"
+
+#End Of Script
