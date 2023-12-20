eb033cf6f13cca6ccb677f084fc89936e23799c4
Matthew Polk <153921961+exjaynine@users.noreply.github.com>
2023-12-20 06:36:07 -0500

    Update writersolo.sh
    Fixed Typo

--- writersolo.sh
+++ writersolo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
+#Please open an issue if you have trouble with this script.
 #If you are on wayland, lets allow this to use native wayland instead of the xwayland system
 #Else, just use the X11 backend.
 if [ -z "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
-  zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop/' --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
+  zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop' --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
 else
   zypak-wrapper '/app/extra/opt/WriterSolo/writersolo-desktop' "$@"
 
